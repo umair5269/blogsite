4e51667f446dbcd31a685a5cb169c2afbddf560f
umair5269 <m.umair5269@yahoo.com>
2025-07-16 16:07:32 +0500

    last change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/Authcontext';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 
