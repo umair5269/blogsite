5473327f9341eb8c94ea5f80107c983eecb7789b
umair5269 <m.umair5269@yahoo.com>
2025-07-16 15:58:07 +0500

    name corrected

--- src/pages/PostEditPage.jsx
+++ src/pages/PostEditPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/Authcontext';
 
 function PostEditPage() {
   const { id } = useParams();
